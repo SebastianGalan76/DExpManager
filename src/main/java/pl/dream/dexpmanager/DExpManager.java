74a717e288e2807f6f74eef677a213ed92d192bf
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-03-13 09:36:33 +0100

    init project

---
+++ src/main/java/pl/dream/dexpmanager/DExpManager.java
@@ -0,0 +1,17 @@
+package pl.dream.dexpmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DExpManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
